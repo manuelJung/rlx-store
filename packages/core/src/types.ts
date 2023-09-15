bcad2748fb4c3a8875e2a6339fb63bc8f4679712
manuelJung <manuel.jung.wwi12@gmail.com>
2023-09-15 16:45:01 +0200

    tested weight

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -104,7 +104,7 @@ export type ConsequenceArgs = {
 export type Rule = {
   id: string
   target: string | string[]
-  consequence: (args:ConsequenceArgs) => Promise<void> | void | Action | Promise<Action>
+  consequence: (args:ConsequenceArgs) => Promise<any> | any
   condition?: (args:ConditionArgs) => Boolean
   weight?: number
   position?: RulePosition
---
+++ packages/core/tests/rule/weight.test.ts
@@ -0,0 +1,50 @@
+import setupTest from "../setup-test"
+
+describe('rule -> weight', () => {
+  it('can define rule execution order when two rules have a weight', () => {
+    const c = setupTest()
+    const store = c.createStore({
+      name: 'my-name',
+      actions: { foo: () => state => state }
+    })
+    const list:string[] = []
+    store.addRule({
+      id: 'rule-1', 
+      target: 'my-name/foo',
+      weight: 2, 
+      consequence: () => list.push('a'), 
+    })
+    store.addRule({
+      id: 'rule-2',
+      weight: 1,
+      consequence: () => list.push('b'),
+      target: 'my-name/foo',
+    })
+
+    store.foo()
+    expect(list).toEqual(['b', 'a'])
+  })
+
+  it('always executes after rules without weight', () => {
+    const c = setupTest()
+    const store = c.createStore({
+      name: 'my-name',
+      actions: { foo: () => state => state }
+    })
+    const list:string[] = []
+    store.addRule({
+      id: 'rule-1', 
+      target: 'my-name/foo',
+      weight: 1,
+      consequence: () => list.push('a'), 
+    })
+    store.addRule({
+      id: 'rule-2',
+      consequence: () => list.push('b'),
+      target: 'my-name/foo',
+    })
+
+    store.foo()
+    expect(list).toEqual(['b', 'a'])
+  })
+})
