bc2aaae447181152fc2743648d19c6a965e35199
manuelJung <manuel.jung.wwi12@gmail.com>
2023-10-28 18:33:47 +0200

    refactored

--- packages/core/src/createStoreManager.ts
+++ packages/core/src/createStoreManager.ts
@@ -1,161 +1,138 @@
-import * as t from './types'
-import { createEventContainer } from './utils'
+import * as t from "./types"
+import { createEventContainer } from "./utils"
 
-export default function createStoreManager (args:t.FactoryArgs, managers:t.Managers) {
+export default function createStoreManager(
+  args: t.FactoryArgs,
+  managers: t.Managers
+) {
   const db = new Map<string, t.StoreContainer>()
   let keyCounter = 1
 
   return {
     db,
-    create: (config:t.StoreConfig) => {
-      const storeId = config.name + (config.key ?? args.getInstanceId(() => `${keyCounter++}`))
-      const container = db.get(storeId) ?? (() => {
-        const container:t.StoreContainer = {
-          id: storeId,
-          state: config.state,
-          numParents: 0,
-          subscriptions: [],
-          events: createEventContainer(),
-          store: args.injectFramework({
-            name: config.name,
-            key: config.key,
-            getState: () => container.state,
-            addRule: rule => managers.rule.register(rule, container),
-            subscribe: cb => {
-              container.subscriptions.push(cb)
-              return () => {
-                container.subscriptions = container.subscriptions.filter(fn => fn != cb)
-              }
-            }
-          })
-        }
+    create: (config: t.StoreConfig) => {
+      const storeId =
+        config.name +
+        (config.key ?? args.getInstanceId(() => `${keyCounter++}`))
+      const container =
+        db.get(storeId) ??
+        (() => {
+          const container: t.StoreContainer = {
+            id: storeId,
+            state: config.state,
+            numParents: 0,
+            subscriptions: [],
+            events: createEventContainer(),
+            store: args.injectFramework({
+              name: config.name,
+              key: config.key,
+              getState: () => container.state,
+              addRule: (rule) => managers.rule.register(rule, container),
+              subscribe: (cb) => {
+                container.subscriptions.push(cb)
+                return () => {
+                  container.subscriptions = container.subscriptions.filter(
+                    (fn) => fn != cb
+                  )
+                }
+              },
+            }),
+          }
 
-        // transform actions
-        for(const key in config.actions) {
-          container.store[key] = function (...args:any[]) {
-            const store = this as t.Store
-            /** consequence can attach action execution */
-            const wrapper = store.dispatchWrapper ?? (fn => fn())
-            return wrapper(() => {
-              const updateFn = config.actions[key](...args)
-
-              if(typeof updateFn === 'function') {
-                return managers.rule.dispatch(
+          // transform actions
+          for (const key in config.actions) {
+            container.store[key] = function (...args: any[]) {
+              const store = this as t.Store
+              /** consequence can attach action execution */
+              const wrapper = store.dispatchWrapper ?? ((fn) => fn())
+              const dispatch = (key: string, payload: any, cb: any) => wrapper(() => {
+                managers.rule.dispatch(
                   {
-                    type: config.name + '/' + key,
+                    type: config.name + "/" + key,
                     meta: args,
-                    payload: args[0],
-                  }, 
+                    payload,
+                  },
                   container,
                   db,
-                  () => {
-                    container.state = updateFn(container.state)
-                  },
+                  cb
                 )
-              }
-              else {
-                return new Promise((resolve) => {
-                  managers.rule.dispatch(
-                    {
-                      type: config.name + '/' + key + '/request',
-                      meta: args,
-                      payload: null,
-                    },
-                    container,
-                    db,
-                    () => {
-                      container.state = {
-                        ...container.state,
-                        isFetching: true,
-                        fetchError: null,
-                      }
-                      updateFn.fetcher(container.state).then(
-                        result => managers.rule.dispatch(
-                          {
-                            type: config.name + '/' + key + '/success',
-                            meta: args,
-                            payload: result,
-                          },
-                          container,
-                          db,
-                          () => {
-                            container.state = {
-                              ...container.state,
-                              data: result,
-                              isFetching: false,
-                            }
-                            resolve(true)
-                          }
-                        ),
-                        error => managers.rule.dispatch(
-                          {
-                            type: config.name + '/' + key + '/failure',
-                            meta: args,
-                            payload: {
-                              msg: error.toString(),
-                              stack: error.stack,
-                            },
-                          },
-                          container,
-                          db,
-                          () => {
-                            container.state = {
-                              ...container.state,
-                              fetchError: error.toString(),
-                              isFetching: false,
-                            }
-                            resolve(false)
-                          }
-                        )
-                      )
+              })
+              const updateFn = config.actions[key](...args)
+
+              if (typeof updateFn === "function") {
+                return dispatch(key, args[0], () => container.state = updateFn(container.state))
+              } else {
+                return new Promise(resolve => {
+                  dispatch(key+'/request', null, () => {
+                    container.state = {
+                      ...container.state,
+                      isFetching: true,
+                      fetchError: null,
                     }
-                  )
+                    updateFn.fetcher(container.state).then(
+                      result => dispatch(key+'/success', result, () => {
+                        container.state = {
+                          ...container.state,
+                          data: result,
+                          isFetching: false,
+                        }
+                        resolve(true)
+                      }),
+                      error => dispatch(key+'/failure', error, () => {
+                        container.state = {
+                          ...container.state,
+                          fetchError: error.toString(),
+                          isFetching: false,
+                        }
+                        resolve(false)
+                      })
+                    )
+                  })
                 })
               }
-            })
+            }
           }
-        }
 
-        args.onMount(() => {
-          container.numParents++
-          managers.rule.dispatch(
-            {
-              type: config.name + '/@mount',
-              meta: [],
-              payload: null
-            }, 
-            container,
-            db,
-            () => null,
-          )
-          container.events.trigger({type: 'MOUNT'})
-        })
+          args.onMount(() => {
+            container.numParents++
+            managers.rule.dispatch(
+              {
+                type: config.name + "/@mount",
+                meta: [],
+                payload: null,
+              },
+              container,
+              db,
+              () => null
+            )
+            container.events.trigger({ type: "MOUNT" })
+          })
 
-        args.onDestroy(() => {
-          container.numParents--
-          managers.rule.dispatch(
-            {
-              type: config.name + '/@destroy',
-              meta: [],
-              payload: null
-            }, 
-            container,
-            db,
-            () => null, 
-          )
+          args.onDestroy(() => {
+            container.numParents--
+            managers.rule.dispatch(
+              {
+                type: config.name + "/@destroy",
+                meta: [],
+                payload: null,
+              },
+              container,
+              db,
+              () => null
+            )
 
-          if(container.numParents === 0 && !config.persist) {
-            container.events.trigger({type: 'DESTROY'})
-            db.delete(storeId)
-          }
-        })
+            if (container.numParents === 0 && !config.persist) {
+              container.events.trigger({ type: "DESTROY" })
+              db.delete(storeId)
+            }
+          })
 
-        db.set(storeId, container)
-        managers.events.trigger({type: 'REGISTER_STORE', container})
-        return container
-      })()
+          db.set(storeId, container)
+          managers.events.trigger({ type: "REGISTER_STORE", container })
+          return container
+        })()
 
       return container.store
-    }
+    },
   }
 }
--- packages/core/tests/store/async-action-config.test.ts
+++ packages/core/tests/store/async-action-config.test.ts
@@ -1,7 +1,7 @@
 import setupTest from "../setup-test"
 
 describe('store -> async action config', () => {
-  it('triggers /request and /success action when action was called', async () => {
+  it('triggers /request and /success action when action was called successfull', async () => {
     const c = setupTest({preventAutoMount:true})
     const fetcher = jest.fn(async () => 'result')
     const store = c.createStore({
@@ -23,7 +23,36 @@ describe('store -> async action config', () => {
     )
 
     expect(c.managers.rule.dispatch).toBeCalledWith(
-      {type: 'my-name/increment/success', meta: [], payload: 'result2'},
+      {type: 'my-name/increment/success', meta: [], payload: 'result'},
+      expect.anything(),
+      expect.anything(),
+      expect.anything(),
+    )
+  })
+
+  it('triggers /request and /failure action when action call failed', async () => {
+    const c = setupTest({preventAutoMount:true})
+    const fetcher = jest.fn(async () => {throw new Error('my-error')})
+    const store = c.createStore({
+      name: 'my-name',
+      actions: {
+        increment: () => ({ fetcher })
+      }
+    })
+
+    const result = await store.increment()
+
+    expect(result).toBe(false)
+
+    expect(c.managers.rule.dispatch).toBeCalledWith(
+      {type: 'my-name/increment/request', meta: [], payload: null},
+      expect.anything(),
+      expect.anything(),
+      expect.anything(),
+    )
+
+    expect(c.managers.rule.dispatch).toBeCalledWith(
+      {type: 'my-name/increment/failure', meta: [], payload: new Error('my-error')},
       expect.anything(),
       expect.anything(),
       expect.anything(),
