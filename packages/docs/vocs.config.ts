8a850358ef89aacc09a34283d4d28960823451d6
manuelJung <manuel.jung.wwi12@gmail.com>
2024-05-02 16:53:02 +0200

    setup vocs

---
+++ packages/docs/vocs.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Docs',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'Example',
+      link: '/example',
+    },
+    {
+      text: 'Blog',
+      link: '/blog',
+    }
+  ],
+})
